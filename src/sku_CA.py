2d01772429f026a4a6efa4ebb58dd63c16e18563
Srujana Rao <srujanaraoms@gmail.com>
2022-12-28 17:13:28 -0500

    Add files via upload

--- src/sku_CA.py
+++ src/sku_CA.py
@@ -1,8 +1,9 @@
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import requests
 from getuseragent import UserAgent
 import pandas as pd
 import csv
+import time
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -13,49 +14,57 @@ csv_writer.writerow(['SKU', 'Price', 'Product_Name', 'CDW_Part'])
 data = pd.read_csv('input_file_CA.csv')
 sku_list = data['SKU'].tolist()
 sku_list = [sku for sku in sku_list if str(sku) != 'nan']
-
+price_list = []
 product_names_list = []
 cdw_parts_list = []
-min_price_list = []
-url3_list = []
-records = []
-price_list = []
 
 myuseragent = UserAgent("all", requestsPrefix=True).Random()
 session = requests.Session()
+time.sleep(5)
 
 for sku in sku_list:
-    url_1 = f'https://www.cdw.ca/search/?key={sku}'
-    source = session.get(url_1, headers=myuseragent, allow_redirects=False).text
+    url = f'https://www.cdw.ca/search/?key={sku}'
+    source = session.get(url, headers=myuseragent, allow_redirects=False).text
     soup = BeautifulSoup(source, 'lxml')
 
     try:
         price_url_content = soup.find('div', class_='container').script.text.split('"')[1]
         generic_url = 'https://www.cdw.ca'
         price_url = generic_url + (price_url_content)
-
         source = session.get(price_url, headers=myuseragent, allow_redirects=False).text
         soup = BeautifulSoup(source, 'lxml')
-        price_1 = soup.find('span', class_='price-type-selected').text
-        price_list.append(price_1)
+        price = soup.find('span', class_='price-type-selected').text
         product_name = soup.find('h1', class_='fn').text
-        product_names_list.append(product_name)
         cdw_part = soup.find('span', class_='edc').text
-        cdw_parts_list.append(cdw_part)
-
-        print(sku, price_1, product_name, cdw_part)
-        print()
-        csv_writer.writerow([sku, price_1, product_name, cdw_part])
 
     except Exception as e:
-        price_1 = "SKU Not found"
-        price_list.append(price_1)
-        product_name = "Not applicable"
-        product_names_list.append(product_name)
-        cdw_part = "Not applicable"
-        cdw_parts_list.append(cdw_part)
-        print(sku, "SKU Not found", "Not applicable", "Not applicable")
-        print()
-        csv_writer.writerow([sku, price_1, product_name, cdw_part])
+        try:
+            search_list = soup.find_all('div', class_='search-result coupon-check')
+            sku_found = False
+            for product in search_list:
+                mfg = product.find('span', class_='mfg-code').text
+                mfg = mfg[6:]
+                if sku == mfg:
+                    sku_found = True
+                    price = product.find('div', class_='price-type-price').text
+                    product_name = product.find('a', class_='search-result-product-url').text
+                    cdw_part = product.find('span', class_='cdw-code').text
+                    cdw_part = cdw_part[6:]
+            if not sku_found:
+                price = "SKU exact match not found"
+                product_name = "Not applicable"
+                cdw_part = "Not applicable"
+
+        except Exception as e:
+            price = "SKU exact match not found"
+            product_name = "Not applicable"
+            cdw_part = "Not applicable"
+
+    price_list.append(price)
+    product_names_list.append(product_name)
+    cdw_parts_list.append(cdw_part)
+    print(sku, price, product_name, cdw_part)
+    print()
+    csv_writer.writerow([sku, price, product_name, cdw_part])
 
 csv_file.close()
